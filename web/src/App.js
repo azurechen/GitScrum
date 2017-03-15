24c684f7b4371ef4dace44c3441419404d7034c2
azurechen <azure517981@gmail.com>
2017-03-16 00:38:58 +0800

    Add the Layout of fields.

--- web/src/App.js
+++ web/src/App.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Field from './components/Field';
+
 class App extends Component {
+
+  getFields() {
+    var fields = [];
+    for (var i = 0; i < 4; i++) {
+      fields.push(<Field key={i} />);
+    }
+    return fields;
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Board">
+          {this.getFields()}
+        </div>
       </div>
     );
   }
---
+++ web/src/components/Field.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Field.css';
+
+class Field extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className="Field">
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default Field;
